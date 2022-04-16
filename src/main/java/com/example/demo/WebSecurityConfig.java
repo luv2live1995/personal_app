74d2e1c903ff1002f166e0b4b4430ff42751a7ae
Le Dinh Hoang Son <blackpantherluv2code@gmail.com>
2022-04-16 15:09:49 +0900

    add

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,61 +1,90 @@
 package com.example.demo;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.demo.service.MyUserDetailsService;
 
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-//	@Autowired
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@Autowired
+	
+	@Autowired 
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	
     @Autowired
     private MyUserDetailsService userDetailsService;
+    
+	/*
+	 * @Bean public PasswordEncoder passwordEncoder() { return new
+	 * BCryptPasswordEncoder(); }
+	 */
+    
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception { 
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(bCryptPasswordEncoder); 
+	}
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-	        	.antMatchers("/", "/login", "/registration", "/error").permitAll()
-	        	.antMatchers("/resources/**").permitAll()
-	        	.antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
-	        	.authenticated().and().csrf().disable()
+	        	.mvcMatchers("/", "/login", "/registration", "/error").permitAll() // url that doesn't require auth
+				.mvcMatchers("/", "/js/**", "/css/**", "/bootstrap-4.0.0-dist/css/**", "/bootstrap-4.0.0-dist/js/**").permitAll()
+	        	.mvcMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
+	        	.authenticated().and()
+//	        .requestCache().disable() // For ex, user tended to go to /help URL in logout status, then he login, he is supposed to go to /help page. This is disabled
 	        .formLogin()
-	        	.loginPage("/login").failureUrl("/login?error=true")
+	        	.loginPage("/login")
 	        	.defaultSuccessUrl("/admin/home")
+	        	.failureUrl("/login?error=true")
 	        	.usernameParameter("username")
 	        	.passwordParameter("password")
 	        	.and()
 	        .logout()
 	        	.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-	        	.logoutSuccessUrl("/login").and().exceptionHandling()
+	        	.logoutSuccessUrl("/login")
+	        	.and()
+	        	.exceptionHandling()
 	        	.accessDeniedPage("/access-denied");
 	}
 	
+	
+	/**
+	 * Ignore any 
+	 */
 	@Override
 	public void configure(WebSecurity web) throws Exception { 
 		web
 			.ignoring() 
-			.antMatchers("/resources/**"); 
+			.antMatchers("/images/**", "/css/**"); 
 	}
 	 
 	
